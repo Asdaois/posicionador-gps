f2ebb37d1d146085f251fabafc8adf1044631ba8
Jose Guevara <94gjose@gmail.com>
2023-02-23 10:17:34 -0400

    style: change to spanish

--- main-web/mapa.js
+++ main-web/mapa.js
@@ -2,49 +2,48 @@ const URL = () =>
   "https://api.thingspeak.com/channels/2004262/feeds.json?api_key=A9GEUJVRWAWOVQRJ&results=20&timezone=America/Caracas";
 
 var localizaciones = [];
-var localizacionsPorDias = {};
 
 if (localizaciones.length === 0) {
   fetch(URL())
-    .then((response) => response.json())
-    .then((data) => {
-      const localizaciones = data.feeds.map((feed) => [
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      const localizaciones = datos.feeds.map((feed) => [
         feed.field1,
         feed.field2,
       ]);
-      initWidgetCalendar(getDateInfo(localizaciones));
-      ActualizarLocalizaciones(localizaciones);
+      inicializarWidgetCalendario(getFechas(localizaciones));
+      ON_ActualizarLocalizaciones(localizaciones);
     });
 }
 
-function getDateInfo(localizaciones) {
-  const groupedFeeds = {};
+function getFechas(localizaciones) {
+  const feedsAgrupadas = {};
   localizaciones.forEach((feed) => {
     const date = moment(feed.created_at);
-    const year = date.year();
-    const month = date.month();
-    const day = date.date();
+    const año = date.year();
+    const mes = date.month();
+    const dia = date.date();
 
-    if (!groupedFeeds[year]) {
-      groupedFeeds[year] = {};
+    if (!feedsAgrupadas[año]) {
+      feedsAgrupadas[año] = {};
     }
 
-    if (!groupedFeeds[year][month]) {
-      groupedFeeds[year][month] = {};
+    if (!feedsAgrupadas[año][mes]) {
+      feedsAgrupadas[año][mes] = {};
     }
 
-    if (!groupedFeeds[year][month][day]) {
-      groupedFeeds[year][month][day] = [];
+    if (!feedsAgrupadas[año][mes][dia]) {
+      feedsAgrupadas[año][mes][dia] = [];
     }
 
-    groupedFeeds[year][month][day].push(feed);
+    feedsAgrupadas[año][mes][dia].push(feed);
   });
-  console.log({ groupedFeeds });
-  return groupedFeeds;
+  console.log({ groupedFeeds: feedsAgrupadas });
+  return feedsAgrupadas;
 }
 
 let destroyMap = null
-function ActualizarLocalizaciones(localizaciones) {
+function ON_ActualizarLocalizaciones(localizaciones) {
   if (destroyMap !== null) {
     destroyMap();
   }
@@ -94,9 +93,7 @@ function formatDate(dateStr) {
   return formattedDate;
 }
 
-initWidgetCalendar();
-
-function initWidgetCalendar(data) {
+function inicializarWidgetCalendario(data) {
   const yearSelect = document.getElementById("select-year");
   const monthSelect = document.getElementById("select-month");
   const daySelect = document.getElementById("select-day");
@@ -110,7 +107,7 @@ function initWidgetCalendar(data) {
   }
 
   // Update the month select options when the year changes
-  function updateMonths() {
+  function actualizarMeses() {
     const year = yearSelect.value;
     monthSelect.innerHTML = "";
     for (const month in data[year]) {
@@ -119,13 +116,13 @@ function initWidgetCalendar(data) {
       option.textContent = month;
       monthSelect.appendChild(option);
     }
-    updateDays();
+    actualizarDias();
   }
 
-  yearSelect.addEventListener("change", updateMonths);
+  yearSelect.addEventListener("change", actualizarMeses);
 
   // Update the day select options when the month changes
-  function updateDays() {
+  function actualizarDias() {
     const year = yearSelect.value;
     const month = monthSelect.value;
     daySelect.innerHTML = "";
@@ -138,7 +135,7 @@ function initWidgetCalendar(data) {
     updateData();
   }
 
-  monthSelect.addEventListener("change", updateDays);
+  monthSelect.addEventListener("change", actualizarDias);
 
   // Update the data display when the date changes
   function updateData() {
@@ -147,11 +144,11 @@ function initWidgetCalendar(data) {
     const dia = daySelect.value;
     const localizaciones = data[año][mes][dia];
     console.log("Fecha Seleccionada:", año, mes, dia, localizaciones);
-    ActualizarLocalizaciones(localizaciones);
+    ON_ActualizarLocalizaciones(localizaciones);
   }
 
   daySelect.addEventListener("change", updateData);
 
   // Initialize the month and day select options
-  updateMonths();
+  actualizarMeses();
 }
